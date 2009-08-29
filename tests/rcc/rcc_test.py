5fd8661b1787968e1a2b3036de50f5dbc620c73f
Lauro Neto <lauro.neto@openbossa.org>
2009-08-29 16:52:58 -0300

    Adding test for pyside-rcc4

---
+++ tests/rcc/rcc_test.py
@@ -0,0 +1,23 @@
+
+import unittest
+
+from PySide.QtCore import QFile
+
+import example_rc
+
+class TestRccSimple(unittest.TestCase):
+    def testSimple(self):
+        handle = QFile(":words.txt")
+        handle.open(QFile.ReadOnly)
+        original = open("words.txt", "r")
+        self.assertEqual(handle.readLine(), original.readline())
+
+    def testAlias(self):
+        handle = QFile(":jack")
+        handle.open(QFile.ReadOnly)
+        original = open("shining.txt", "r")
+        self.assertEqual(handle.readLine(), original.readline())
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/rcc/run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This is a nasty workaround of a CTest limitation
+# of setting the environment variables for the test.
+
+# $1: python executable
+# $2: python test
+# $3: qrc file
+
+export PYTHONPATH=$PYTHON_PATH:`pwd`
+pyside-rcc4 -o `basename $3 .qrc`_rc.py $3
+cd $4
+$1 $2
