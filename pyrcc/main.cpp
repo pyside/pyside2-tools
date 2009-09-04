588f9bb949739be9386cd3f4e37ed0817ea5fa0a
Lauro Neto <lauro.neto@openbossa.org>
2009-09-04 10:49:45 -0300

    Fixing license header for rcc. Fixes #32

--- pyrcc/main.cpp
+++ pyrcc/main.cpp
@@ -1,26 +1,27 @@
-/****************************************************************************
-**
-** Copyright (C) 1992-2005 Trolltech AS. All rights reserved.
-** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(ies)
-**
-** Contact: PySide team <contact@pyside.org>
-**
-** This file is part of the tools applications of the Qt Toolkit.
-**
-** Licensees holding a valid Qt License Agreement may use this file in
-** accordance with the rights, responsibilities and obligations
-** contained therein.  Please consult your licensing agreement or
-** contact sales@trolltech.com if any conditions of this licensing
-** agreement are not clear to you.
-**
-** Further information about Qt licensing is available at:
-** http://www.trolltech.com/products/qt/licensing.html or by
-** contacting info@trolltech.com.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-****************************************************************************/
+/*
+ * This file is part of the API Extractor project.
+ *
+ * Copyright (C) 1992-2005 Trolltech AS. All rights reserved.
+ * Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * Contact: PySide team <contact@pyside.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
 
 #include <qstring.h>
 #include <qstringlist.h>
