52301a1c53c903443cf1459c9e0335077494e667
Marcelo Lira <marcelo.lira@openbossa.org>
2011-06-27 19:08:24 -0300

    Fixed the tests for pyside-rcc and added a new test for bug #901.

--- tests/rcc/rcc_test.py
+++ tests/rcc/rcc_test.py
@@ -1,27 +1,29 @@
-
+import os
 import unittest
-
 from PySide.QtCore import QFile
-
 import example_rc
 
 class TestRccSimple(unittest.TestCase):
+
+    def setUp(self):
+        self.testdir = os.path.dirname(__file__)
+
     def testSimple(self):
         handle = QFile(":words.txt")
         handle.open(QFile.ReadOnly)
-        original = open("words.txt", "r")
+        original = open(os.path.join(self.testdir, "words.txt"), "r")
         self.assertEqual(handle.readLine(), original.readline())
 
     def testAlias(self):
         handle = QFile(":jack")
         handle.open(QFile.ReadOnly)
-        original = open("shining.txt", "r")
+        original = open(os.path.join(self.testdir, "shining.txt"), "r")
         self.assertEqual(handle.readLine(), original.readline())
 
     def testHuge(self):
         handle = QFile(":manychars.txt")
         handle.open(QFile.ReadOnly)
-        original = open("manychars.txt", "r")
+        original = open(os.path.join(self.testdir, "manychars.txt"), "r")
         self.assertEqual(handle.readLine(), original.readline())
 
 
--- tests/rcc/run_test.sh
+++ tests/rcc/run_test.sh
@@ -3,11 +3,11 @@
 # This is a nasty workaround of a CTest limitation
 # of setting the environment variables for the test.
 
-# $1: python executable
+# $1: pyside-rcc
 # $2: python test
 # $3: qrc file
 
 export PYTHONPATH=$PYTHONPATH:`pwd`
-pyside-rcc -o `basename $3 .qrc`_rc.py $3
-cd $4
-$1 $2
+$1 -o `basename $3 .qrc`_rc.py $3
+`pkg-config shiboken --variable=python_interpreter` $2
+
